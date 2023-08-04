cc9ac0ad0e675735a41b73796eb127f5188410ea
Turkey323 <58702346+Turkey323@users.noreply.github.com>
2023-08-04 05:11:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,18 +4,7 @@ tg.expand();
 
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
-async function start() {
-	
-olditemslist = document.getElementsByClassName("inner")[0];
-response = await fetch("1.json");
-item = await response.json();
-temptext = "";
-for (let index = 0; index < item.length; index++) {
-    console.log(item[index].itemname);
-	temptext += '<div class="item"><img src="' + item[index].id + '.png" alt="" class="img"> <p><center>' + item[index].itemname + '</center></p><button class="btn" id="'+item[index].id + '">Добавить</button></div>'
-}
-olditemslist.innerHTML = temptext;
-}
+
 let item = "";
 var elements = document.getElementsByClassName("btn");
 
@@ -33,16 +22,6 @@ for (var i = 0; i < elements.length; i++) {
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
-
-
-
-(async() => {
-  console.log('before start');
-
-  await start();
-  
-  console.log('after start');
-})();
 
 
 
