0a166f1e08b3881c1cd7e63fd92ad6e4f9a76fa9
Turkey323 <58702346+Turkey323@users.noreply.github.com>
2023-08-04 14:39:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var elements = document.getElementsByClassName("btn");
 var ShowChests = function(){
   console.log("Show chests");
   HideAll();
-  var elms = document.querySelectorAll("[id*='Chests']");
+  var elms = document.querySelectorAll("[id*='Chests_']");
 for(var i = 0; i < elms.length; i++) 
   elms[i].parentElement.style.display='inline-block'; 
 };
