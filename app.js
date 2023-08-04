30a3dd25b4ddec1dcff291e7a3b3776a4e59fc9f
Turkey323 <58702346+Turkey323@users.noreply.github.com>
2023-08-04 05:05:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
+olditemslist = document.getElementsByClassName("inner")[0];
+response = await fetch("1.json");
+item = await response.json();
+temptext = "";
+for (let index = 0; index < item.length; index++) {
+    console.log(item[index].itemname);
+	temptext += '<div class="item"><img src="' + item[index].id + '.png" alt="" class="img"> <p><center>' + item[index].itemname + '</center></p><button class="btn" id="'+item[index].id + '">Добавить</button></div>'
+}
+olditemslist.innerHTML = temptext;
+
 let item = "";
 var elements = document.getElementsByClassName("btn");
 
